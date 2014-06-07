ed7fee9dd09b5655562fdef9ed49f27276071054
datateller@126.com <datateller@126.com>
2014-06-07 06:39:02 +0000

    from server

--- consumption/db.sh
+++ consumption/db.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+export PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 source /etc/profile
 today=`date +%Y%m%d`
 python3 /home/shengeng/workspace/ywbserver/utils/import_cons_db.py > /var/log/ywblog/consdb_${today}.log
