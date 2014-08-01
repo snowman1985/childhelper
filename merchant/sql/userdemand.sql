792bf12741ec2889e342e63bfe153e964ea2f5ef
datateller@126.com <datateller@126.com>
2014-08-01 15:37:34 +0000

    initial data sql for userdemand

---
+++ merchant/sql/userdemand.sql
@@ -0,0 +1,2 @@
+INSERT INTO merchant_userdemand (user_id, content, pub_time) values(35, '我想找一个教孩子拉小提琴的老师', '2014-09-20 10:20');
+INSERT INTO merchant_userdemand (user_id, content, pub_time) values(35, '我想为孩子找一个教数学的老师', '2014-09-21 10:20');
